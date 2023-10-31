679b6bcb11fbf3dbcfb4d61feace07547e2bb100
BigtronicaSAS <sistemasysoporte@bigtronica.com>
2023-10-31 10:15:37 -0500

    mapeo de servos

---
+++ examples/ServoTest/ServoTest.ino
@@ -0,0 +1,24 @@
+#include <Curiosity.h>
+#include <Servo.h>
+Bot bot; 
+Servo LYservo;
+Servo RXservo;
+Servo RYservo;
+Servo LXservo;
+
+void setup(){
+LYservo.attach(9);
+RXservo.attach(6);
+RYservo.attach(3);
+LXservo.attach(5);
+bot.setup();
+}
+void loop(){
+bot.play();
+bot.read_gamepad();
+RXservo.write(map(ps2x.Analog(PSS_RX), 0, 255, 0, 179));
+RYservo.write(map(ps2x.Analog(PSS_RY), 0, 255, 0, 179));
+LXservo.write(map(ps2x.Analog(PSS_LX), 0, 255, 0, 179)); 
+LYservo.write(map(ps2x.Analog(PSS_LY), 0, 255, 0, 179));
+delay(50);
+}
