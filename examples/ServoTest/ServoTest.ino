12fef72c28f7c610d203ba755b4dab2d6b307f28
BigtronicaSAS <sistemasysoporte@bigtronica.com>
2023-10-31 10:50:22 -0500

    error mapeo

--- examples/ServoTest/ServoTest.ino
+++ examples/ServoTest/ServoTest.ino
@@ -16,9 +16,9 @@ bot.setup();
 void loop(){
 bot.play();
 bot.read_gamepad();
-RXservo.write(map(ps2x.Analog(PSS_RX), 0, 255, 0, 179));
-RYservo.write(map(ps2x.Analog(PSS_RY), 0, 255, 0, 179));
-LXservo.write(map(ps2x.Analog(PSS_LX), 0, 255, 0, 179)); 
-LYservo.write(map(ps2x.Analog(PSS_LY), 0, 255, 0, 179));
+RXservo.write(map(bot.Analog(PSS_RX), 0, 255, 0, 179));
+RYservo.write(map(bot.Analog(PSS_RY), 0, 255, 0, 179));
+LXservo.write(map(bot.Analog(PSS_LX), 0, 255, 0, 179)); 
+LYservo.write(map(bot.Analog(PSS_LY), 0, 255, 0, 179));
 delay(50);
 }
